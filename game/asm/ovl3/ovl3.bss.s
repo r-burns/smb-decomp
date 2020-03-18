f5daf4cf6534c91c2eb7fdc6ea69dac340e637b4
tehz <tehzz@users.noreply.github.com>
2020-03-18 17:46:07 -0400

    add ovl3

---
+++ game/asm/ovl3/ovl3.bss.s
@@ -0,0 +1,71 @@
+.include "macros.inc"
+
+.section .bss
+
+# 0x8018CF80 -> 0x8018D0C0 [0x140 bytes]
+glabel D_ovl3_8018CF80
+    .space 16
+glabel D_ovl3_8018CF90
+    .space 4
+glabel D_ovl3_8018CF94
+    .space 4
+glabel D_ovl3_8018CF98
+    .space 4
+glabel D_ovl3_8018CF9C
+    .space 4
+glabel D_ovl3_8018CFA0
+    .space 4
+glabel D_ovl3_8018CFA4
+    .space 4
+glabel D_ovl3_8018CFA8
+    .space 4
+glabel D_ovl3_8018CFAC
+    .space 4
+glabel D_ovl3_8018CFB0
+    .space 4
+glabel D_ovl3_8018CFB4
+    .space 4
+glabel D_ovl3_8018CFB8
+    .space 4
+glabel D_ovl3_8018CFBC
+    .space 2
+glabel D_ovl3_8018CFBE
+    .space 2
+glabel D_ovl3_8018CFC0
+    .space 8
+glabel D_ovl3_8018CFC8
+    .space 20
+glabel D_ovl3_8018CFDC
+    .space 4
+glabel D_ovl3_8018CFE0
+    .space 4
+glabel D_ovl3_8018CFE4
+    .space 4
+glabel D_ovl3_8018CFE8
+    .space 8
+glabel D_ovl3_8018CFF0
+    .space 4
+glabel D_ovl3_8018CFF4
+    .space 4
+glabel D_ovl3_8018CFF8
+    .space 72
+glabel D_ovl3_8018D040
+    .space 4
+glabel D_ovl3_8018D044
+    .space 4
+glabel D_ovl3_8018D048
+    .space 24
+glabel D_ovl3_8018D060
+    .space 48
+glabel D_ovl3_8018D090
+    .space 4
+glabel D_ovl3_8018D094
+    .space 4
+glabel D_ovl3_8018D098
+    .space 4
+glabel D_ovl3_8018D09C
+    .space 4
+glabel D_ovl3_8018D0A0
+    .space 4
+glabel D_ovl3_8018D0A4
+    .space 28
