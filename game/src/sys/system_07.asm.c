bc9a6f5340ec43edf2c0fe1789002b6b581eaca1
tehz <5674391-tehz@users.noreply.gitlab.com>
2021-11-29 14:57:44 -0500

    move system_07 into C

---
+++ game/src/sys/system_07.asm.c
@@ -0,0 +1,55 @@
+#include <PR/ultratypes.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_80018EE0.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_80018F7C.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_80018FBC.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_80018FF4.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_8001902C.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_800190B0.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_800191FC.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_800192D0.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_80019438.s")
+#endif
+
+#ifdef MIPS_TO_C
+#else
+#pragma GLOBAL_ASM("game/nonmatching/system_07/func_80019980.s")
+#endif
+#pragma GCC diagnostic pop
