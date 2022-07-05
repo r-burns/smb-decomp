8404ed9e45f0a486084f9b831f12b2f7faf28b5d
tehz <5674391-tehz@users.noreply.gitlab.com>
2022-07-05 18:11:09 -0400

    update diff.py (now can rebuild with `-wm`)

--- utils/diff_settings.py
+++ utils/diff_settings.py
@@ -9,7 +9,15 @@ def add_custom_arguments(parser):
 def apply(config, args):
     lang = args.lang or 'us'
 
-    config['baseimg'] = f'baserom.{lang}.z64'
-    config['myimg'] = f'build/{lang}/ssb64.{lang}.z64'
-    config['mapfile'] = f'build/{lang}/ssb64.{lang}.map'
-    config['source_directories'] = ['game/src', 'game/include']
+    config["baseimg"] = f'baserom.{lang}.z64'
+    config["myimg"] = f'build/{lang}/ssb64.{lang}.z64'
+    config["mapfile"] = f'build/{lang}/ssb64.{lang}.map'
+    config["source_directories"] = ['game/src']
+    config["show_line_numbers_default"] = True
+    config["arch"] = "mips"
+    config["map_format"] = "gnu" # gnu or mw
+    config["make_command"] = ['doit']
+    # config["mw_build_dir"] = "build/" # only needed for mw map format
+    # config["makeflags"] = []
+    # config["objdump_executable"] = ""
+
